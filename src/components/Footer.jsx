5c4625807ac3f9463d3acb46c461e5a2b364a544
Takalani Nedzanani <135524369+Takalani-Nedzanani@users.noreply.github.com>
2025-02-21 21:19:33 +0200

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import '../Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Book World. All rights reserved.</p>
+      <p>&copy; 2024 Takalani Nedzanani. All rights reserved.</p>
     </footer>
   );
 };
